122f36625de0b949d43a54b33a9855db22dd1b87
Andriy Bats <abats333@gmail.com>
2021-09-28 22:33:26 +0300

    add folder Homework13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Homework13Component } from './homework13/homework13.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Homework13Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
